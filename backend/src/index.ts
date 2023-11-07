07c76897e822a58f1b8dd988fa8a2f6d2a1cc543
Plamena Mihaylova <plamena.pavlova.mihaylova@gmail.com>
2023-11-07 14:55:18 +0200

    Add initial implementation of sorting functionality.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -28,6 +28,31 @@ app.get('/api/cities/density', (req: Request, res: Response) => {
     return res.send(citiesWithDensity);
 })
 
+app.get('/api/sort/:order/:property', (req: Request, res: Response) => {
+    const sortProperty: any = req.params.property
+    const sortOrder: string = req.params.order
+    const sortedCities = sortCities(sortOrder, sortProperty)
+    return res.send(sortedCities)
+})
+
+
+const sortCities = (sortOrder: string, sortProperty: keyof ICity): ICity[] => {
+    if (!(sortProperty in cities[0])) {
+        throw new Error('Invalid sorting parameter')
+    }
+    // add error handling for the cases when sort order is not the correct value
+    return cities.sort((city1, city2) => {
+        const city1Value = city1[sortProperty] ?? '';
+        const city2Value = city2[sortProperty] ?? '';
+
+        if (sortOrder === 'asc') {
+            return city1Value < city2Value ? -1 : 1;
+        }
+
+        return city1Value < city2Value ? 1 : -1;
+    })
+}
+
 app.get("/api/cities/filter/:searchTerm", (req: Request, res: Response) => {
     const searchTerm = req.params.searchTerm;
     const filteredCities = cities.reduce((accumulator: ICity[], city: ICity) => {
